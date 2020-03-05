277eac3b2ed0c660326a379bfd939a45748dfaea
ALexanderSpiridonov <spiridonov.alexandr@gmail.com>
2020-03-05 20:08:40 +0100

    login system without database connections

--- my_first_flask_app.py
+++ my_first_flask_app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app=Flask(__name__)
+
+app.config['SECRET_KEY'] = '60d198c06f34330635165c34358eedc4'
 
 posts = [
     {
@@ -25,6 +27,25 @@ def home():
 @app.route("/about") # home page 
 def about():
     return render_template('about.html')
+
+@app.route("/register", methods=['GET', 'POST']) # registration page 
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST']) # home page 
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'asp@foss.dk' and form.password.data == '12345':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccesssful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)

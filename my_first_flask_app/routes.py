aa5e5ce4da4831a0a44c82752fa6c47d87cf1950
ALexanderSpiridonov <spiridonov.alexandr@gmail.com>
2020-04-08 23:37:38 +0200

    redirection to accout page after login

--- my_first_flask_app/routes.py
+++ my_first_flask_app/routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from my_first_flask_app import app, db, bcrypt
 from my_first_flask_app.models import User, Post
 from my_first_flask_app.forms import RegistrationForm, LoginForm
@@ -71,7 +71,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))  
         else:
             flash('Login Unsuccesssful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
